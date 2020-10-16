3f59eb56aa93373e1ba900de7f1deb7a961276da
mlgmontage <kettleofrassel1@gmail.com>
2020-10-16 19:50:42 +0500

    initial commit

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    class MainClass {
+      public static void Main (string[] args) {
+        for(int i = 0; i< 20; i++) {
+          Console.WriteLine("Fuck you!");
+        }
+      }
+    }
+}
